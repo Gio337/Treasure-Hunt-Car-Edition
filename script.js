26f7ce20b02e84dabdfb0275aef3338ea56d2b15
Gio337 <60225077+Gio337@users.noreply.github.com>
2020-01-23 08:59:06 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,17 @@
+$(".text").click(function() {
+    $(".CAR").fadeOut();
+     $(".text").text("Now click the car key");
+     
+     
+    
+});
+
+$("img").click(function() {
+    $(".key").toggle();
+});
+
+$(".text").click(function() {
+    
+    
+    
+});
